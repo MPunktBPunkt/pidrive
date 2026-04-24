8a443bc3a159355f36241df77a8a27fa3eedd822
MPunktBPunkt <36735679+MPunktBPunkt@users.noreply.github.com>
2026-04-24 12:02:25 +0200

    0.9.14

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ err()  { echo -e "${RED}  ✗ ${1}${NC}"; }
 echo -e "${BOLD}${BLUE}"
 cat << 'EOF'
 ╔═══════════════════════════════════════════╗
-║        PiDrive Installer v0.9.13           ║
+║        PiDrive Installer v0.9.14           ║
 ║   github.com/MPunktBPunkt/pidrive         ║
 ╚═══════════════════════════════════════════╝
 EOF
