5957776b2796403391abe54c25882a8381b6e6dd
MPunktBPunkt <36735679+MPunktBPunkt@users.noreply.github.com>
2026-05-08 21:46:43 +0200

    0.10.53

--- pidrive/modules/bluetooth/bluetooth.py
+++ pidrive/modules/bluetooth/bluetooth.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-modules/bluetooth.py — Public API Facade  v0.10.52
+modules/bluetooth.py — Public API Facade  v0.10.53
 
 Ausgelagert in:
   bt_helpers.py   — Basis-Helfer, Konstanten, Adapter-Steuerung
